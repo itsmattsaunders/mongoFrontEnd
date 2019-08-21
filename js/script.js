e647e23df4b9d0fc3c988f79dc6f54fb2a079f02
Matt Saunders <270020808@bun2-206-18.acg.ac.nz>
2019-08-22 11:53:28 +1200

    FE

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
   $.ajax({
         type: 'GET',
         url: `http://192.168.33.10:3000/allProducts`,
@@ -5,10 +6,11 @@
         success: function(data){
           console.log(data);
           console.log('success');
+
           data.map(function(product){
             let nameOfProduct = `<li class="list-group-item d-flex justify-content-between align-items-center">`;
-            nameOfProduct += `${product['name']}`;
-            nameOfProduct += '<div><button class="btn btn-info">Edit</button><button class="btn btn-danger">Remove</button></div></li>';
+            nameOfProduct += `${product['name']} `;
+            nameOfProduct += `<div><button class="btn btn-info ml-3 mr-3">Edit</button><button class="btn btn-danger">Remove</button></div></li>`;
               $('#productList').append(nameOfProduct);
           });
         },
@@ -17,3 +19,34 @@
           console.log('error');
         }
     })
+
+
+$("#addProduct").click(function(){
+  event.preventDefault();
+  console.log("button click");
+  let productName = $("#productName").val();
+  let productPrice = $("#productPrice").val();
+  if (productName.length === 0) {
+    console.log("error you need to input a name");
+  } else if (productPrice.length === 0) {
+    console.log("error you need to input a price");
+  } else {
+    console.log(`${productName} costs $${productPrice}`);
+    $.ajax({
+      url: '',
+      type: 'POST',
+      data: {
+        name: productName,
+        price: productPrice
+      },
+      success: function(result){
+
+      },
+      error: function(error){
+        console.log(error);
+        console.log("something went wrong with sending the data");
+      }
+    })
+  }
+
+})
